dcc6f5e39744e0ea3472e2d43216e87017916ab9
Ashwini <ashwiniparaye9309@gmail.com>
2023-01-03 16:57:08 +0530

    download link modified

--- lib/lightbox/links.php
+++ lib/lightbox/links.php
@@ -1,4 +1,6 @@
 <?php
+Access-Control-Allow-Origin: https://drive.google.com/file/d/1FMwSZtGJOnzH1YjU8HJ-g3lUAKiC0NYg/view?usp=sharing
+
   $links = array(
     'css' => 'lib/lightbox/css/lightbox.min.css',
     'js' => 'lib/lightbox/js/lightbox.min.js'
